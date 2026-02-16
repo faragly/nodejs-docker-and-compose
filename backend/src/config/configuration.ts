551ff0d5d647b66247f5a30af425115b287de1c7
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2026-02-16 12:27:23 +0300

    added frontend and backend

---
+++ backend/src/config/configuration.ts
@@ -0,0 +1,20 @@
+export default () => ({
+  db: {
+    autoloadEntities: Boolean(process.env.DATABASE_AUTOLOAD_ENTITIES),
+    host: process.env.DATABASE_HOST ?? 'localhost',
+    name: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
+      ? parseInt(process.env.DATABASE_PORT, 10)
+      : 5432,
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    username: process.env.DATABASE_USERNAME,
+  },
+  jwt: {
+    secretKey: process.env.JWT_PRIVATE_KEY,
+    tokenTimeLimit: process.env.JWT_TOKEN_TIME_LIMIT,
+  },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  },
+});
